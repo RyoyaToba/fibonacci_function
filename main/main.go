791cde68d96246c57a9adfb054e57d1e919ac3c4
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2025-07-24 22:02:04 +0900

    キャッシュの考え方を採用し、処理を高速化する

--- main/main.go
+++ main/main.go
@@ -1,23 +1,39 @@
 package main
 
+import "fmt"
+
 // フィボナッチ関数
 func get_fibonacci(n int) (ans int) {
 
-	// n = 0であれば0を返す
-	if n == 0 {
-		return 0
-	}
+	// 値を保管しておくためのキャッシュを用意
+	cash := make(map[int]int)
 
-	// n = 1であれば1を返す
-	if n == 1 {
-		return 1
+	var helper func(int) int
+
+	helper = func(k int) int {
+		// キャッシュに値が存在すればその値を返す
+		if v, ok := cash[k]; ok {
+			return v
+		}
+
+		if k == 0 {
+			return 0
+		}
+
+		if k == 1 {
+			return 1
+		}
+		// キャッシュに値を保存する
+		cash[k] = helper(k-1) + helper(k-2)
+		return cash[k]
 	}
 
 	// 再帰処理として実装を考える
-	return get_fibonacci(n-1) + get_fibonacci(n-2)
+	return helper(n)
 }
 
 // エントリーポイント(コマンドでの実行想定はないが、一応)
 func main() {
-
+	ans := get_fibonacci(100)
+	fmt.Println(ans)
 }
