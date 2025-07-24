ddba65d9329cabcf35d6fa8343a1094572c283ca
RyoyaToba <ryoya.toba@rakus-partners.co.jp>
2025-07-24 19:32:52 +0900

    愚直に実装してみる

--- main/main.go
+++ main/main.go
@@ -1,9 +1,24 @@
 package main
 
 // フィボナッチ関数
-func get_fibonacci(input int) int {
-	// モック
-	return 0
+func get_fibonacci(n int) (ans int) {
+
+	// n = 0であれば0を返す
+	if n == 0 {
+		return 0
+	}
+
+	// n = 1であれば1を返す
+	if n == 1 {
+		return 1
+	}
+
+	// 再帰処理として実装を考える
+	if n >= 2 {
+		ans = get_fibonacci(n-1) + get_fibonacci(n-2)
+	}
+
+	return ans
 }
 
 // エントリーポイント(コマンドでの実行想定はないが、一応)
